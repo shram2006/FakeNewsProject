0c8d728fb796e9539ef91a9a648ea472903440d6
shram2006 <jshramvenkat@gmail.com>
2026-02-26 21:34:59 +0530

    Update app run configuration for deployment

--- app.py
+++ app.py
@@ -109,5 +109,4 @@ def dashboard():
     return render_template("dashboard.html", articles=results)
 
 if __name__ == "__main__":
-    app.run(debug=True)
-    
+    app.run(host="0.0.0.0", port=10000)
